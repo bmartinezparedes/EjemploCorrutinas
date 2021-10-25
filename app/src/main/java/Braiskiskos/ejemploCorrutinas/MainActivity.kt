3c967ecde683ba148b10090cde0e3249ea9cc582
Braiskiskos <bmartinezparedes@danielcastelao.org>
2021-10-25 09:15:15 +0200

    accion de pulsar el boton + launch

--- app/src/main/java/Braiskiskos/ejemploCorrutinas/MainActivity.kt
+++ app/src/main/java/Braiskiskos/ejemploCorrutinas/MainActivity.kt
@@ -2,10 +2,26 @@ package Braiskiskos.ejemploCorrutinas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val miBoton: Button = findViewById(R.id.miBoton)
+
+        miBoton.setOnClickListener {
+        lanzarCorrutina()
+        }
+    }
+    private fun lanzarCorrutina(){
+
+        val job= GlobalScope.launch(Dispatchers.Main){
+
+        }
     }
 }
